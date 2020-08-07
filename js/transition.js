823e993408140a52dcec45981653eed283cbbd17
MahammadKasim Nadim <Mahammadkasim.Nadim@cartus.com>
2020-08-07 21:01:17 +0530

    transition added to pages

---
+++ js/transition.js
@@ -0,0 +1,21 @@
+import highway from '@dogstudio/highway';
+import { TimelineLite } from 'gsap';
+
+class Fade extends highway.Transition {
+    in({ from, to, done }) {
+        const tl = new TimelineLite();
+        tl.fromTo(to, 0.5, { left: '-100%', top: '50%' }, { left: '0%' })
+            .fromTo(to, 0.5, { height: '1vh' }, {
+                height: '90vh', top: '10%', onComplete: () => {
+                    from.remove();
+                    done();
+                }
+            })
+            .fromTo(to.children[0], 1, { opacity: 0 }, { opacity: 1 })
+    }
+    out({ from, done }) {
+        done();
+    }
+}
+
+export default Fade;
